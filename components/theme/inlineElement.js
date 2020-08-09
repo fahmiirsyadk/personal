565cdc78b59bb139ba7d026b25bbb36300b78f7b
Fahmi <fahmiirsyad10@gmail.com>
2020-08-09 11:54:37 +0700

    using manual minified code

--- components/theme/inlineElement.js
+++ components/theme/inlineElement.js
@@ -1,43 +1,8 @@
-import Terser from "terser";
-
 import { colors } from "./constants.bs";
 
-function setColorsByTheme() {
-  const colors = "🌈";
-
-  const mql = window.matchMedia("(prefers-color-scheme: dark)");
-  const prefersDarkFromMQ = mql.matches;
-  const storage = localStorage.getItem("darkMode");
-
-  let colorMode = "light";
-
-  colorMode =
-    storage !== null
-      ? JSON.parse(storage)
-        ? "dark"
-        : "light"
-      : prefersDarkFromMQ
-      ? "dark"
-      : "light";
-
-  const root = document.documentElement;
-
-  Object.entries(colors).forEach(([name, colorByTheme]) => {
-    const cssVarName = `--color-${name}`;
-
-    root.style.setProperty(cssVarName, colorByTheme[colorMode]);
-  });
-}
-
 export function MagicScriptTag() {
-  const boundFn = String(setColorsByTheme).replace(
-    '"🌈"',
-    JSON.stringify(colors)
-  );
-
-  let calledFunction = `(${boundFn})()`;
-  calledFunction = Terser.minify(calledFunction).code;
-  return <script dangerouslySetInnerHTML={{ __html: calledFunction }} />;
+  let minifiedCode = '!function(){const e=window.matchMedia("(prefers-color-scheme: dark)").matches,l=localStorage.getItem("darkMode");let d="light";d=null!==l?JSON.parse(l)?"dark":"light":e?"dark":"light";const h=document.documentElement;Object.entries({text:{light:"hsl(0deg, 0%, 10%)",dark:"hsl(0deg, 0%, 95%)"},foreground:{light:"hsl(0, 0%, 6%)",dark:"hsl(0, 0%, 95%)"},background:{light:"hsl(0, 0%, 95%)",dark:"hsl(0, 0%, 6%)"},primary:{light:"hsl(340deg, 100%, 40%)",dark:"hsl(50deg, 100%, 50%)"},secondary:{light:"hsl(250deg, 100%, 50%)",dark:"hsl(190deg, 100%, 40%)"},gray300:{light:"hsl(0deg, 0%, 70%)",dark:"hsl(0deg, 0%, 30%)"},gray500:{light:"hsl(0deg, 0%, 50%)",dark:"hsl(0deg, 0%, 50%)"},gray700:{light:"hsl(0deg, 0%, 30%)",dark:"hsl(0deg, 0%, 70%)"}}).forEach(([e,l])=>{const r="--color-"+e;h.style.setProperty(r,l[d])})}();'
+  return <script dangerouslySetInnerHTML={{ __html: minifiedCode }} />;  
 }
 
 export function FallbackStyles() {
